76f7151e04d5157a4c4e5c5f51a3925d18112311
nidhi8686 <goyalnidhi8686@gmail.com>
2020-10-08 14:31:57 +0530

    End-of-file & Int to string

---
+++ Java End-of-file/Solution1.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int i=1;
+        Scanner sc=new Scanner(System.in);
+        while(sc.hasNext())
+        {
+            String s=sc.nextLine();
+            System.out.println(i+" "+s);
+            i++;
+        }
+    }
+}
