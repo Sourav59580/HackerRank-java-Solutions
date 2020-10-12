c6427b77bcef668e347f2239e3b46ed9082e300f
abhinav13872 <abhinav13872@gmail.com>
2020-10-12 18:11:41 +0530

    12 Oct

---
+++ Valid Username Regular Expression/Solution1.java
@@ -0,0 +1,13 @@
+
+class UsernameValidator {
+    /*
+     * Write regular expression here.
+     */
+    public static final String regularExpression = "^[a-zA-Z]\\w{7,29}$";
+    /*
+    \w tells alphanumeric characters i.e [a-zA-Z0-9_]
+    since first character is fixed range of characters will be from 7 to 29
+    */
+}
+
+
